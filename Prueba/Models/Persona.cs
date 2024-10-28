48a1b504458d6f2e83dca13217b06fedab0d424f
sebast1213123 <juan.cadena.ramirez@udla.edu.ec>
2024-10-28 07:45:53 -0500

    5 DateTimes

--- Prueba/Models/Persona.cs
+++ Prueba/Models/Persona.cs
@@ -1,14 +1,23 @@
-﻿namespace Prueba.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prueba.Models
 {
     public class Persona
     {
 
 
+        [Key]
 
+        [StringLength(100)]
         public string Apellido { get; set; }
+        [Required]
         public int Edad { get; set;}
+
+        [Range(0, 1000)]
         public float Altura { get; set;}
+        [MaxLength(100)]
         public string Id { get; set;}
+        [Required]
         public DateTime Fecha { get; set;}
     }
 }
