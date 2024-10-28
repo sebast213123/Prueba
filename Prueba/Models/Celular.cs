6c132c0eae70cb8aa8761cfdc8999425c8da5d9a
sebast1213123 <juan.cadena.ramirez@udla.edu.ec>
2024-10-28 07:56:06 -0500

    Segunda Clase con las validaciones

---
+++ Prueba/Models/Celular.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Prueba.Models
+{
+    public class Celular
+    {
+        [Key]
+        [Range(0, 1000)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(500)]
+
+        public string Modelo { get; set; }
+        [Required]
+        [MaxLength(2024)]
+        public int Año { get; set; }
+        [Required]
+        [Range (0, 100000)]
+        public int Precio { get; set; }
+          
+
+    }
+}
