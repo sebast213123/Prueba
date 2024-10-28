8a5792552bca0957b3a9d9d34fa4e46ca64e67bb
sebast1213123 <juan.cadena.ramirez@udla.edu.ec>
2024-10-28 08:36:19 -0500

    Controladores

---
+++ Prueba/Data/PruebaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Prueba.Models;
+
+namespace Prueba.Data
+{
+    public class PruebaContext : DbContext
+    {
+        public PruebaContext (DbContextOptions<PruebaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Prueba.Models.Celular> Celular { get; set; } = default!;
+        public DbSet<Prueba.Models.Persona> Persona { get; set; } = default!;
+    }
+}
--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Prueba.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<PruebaContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PruebaContext") ?? throw new InvalidOperationException("Connection string 'PruebaContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
