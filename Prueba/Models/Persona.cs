df6adb9cabb129fc26385c2db19fbc360a672e6c
sebast1213123 <juan.cadena.ramirez@udla.edu.ec>
2024-10-28 07:38:42 -0500

    Primer Commit

---
+++ Prueba/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿namespace Prueba.Models
+{
+    public class Persona
+    {
+
+
+
+        public string Apellido { get; set; }
+        public int Edad { get; set;}
+        public float Altura { get; set;}
+        public string Id { get; set;}
+        public DateTime Fecha { get; set;}
+    }ffg
+}
